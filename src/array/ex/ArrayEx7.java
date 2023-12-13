047346a72fc5f730e702723cc3f13c13ebfbf84b
ddj82 <dnwnrudckf77@gmail.com>
2023-12-13 20:41:21 +0900

    자율학습03 (영한강의)

---
+++ src/array/ex/ArrayEx7.java
@@ -0,0 +1,15 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[][] students = new int[4][3];
+
+        System.out.println("1번 학생의 성적을 입력하세요:");
+        input.nextInt();
+
+    }
+}
