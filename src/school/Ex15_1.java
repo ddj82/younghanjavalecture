81dd243b6c536f578e81d45288eee210135742f2
ddj82 <dnwnrudckf77@gmail.com>
2023-12-13 19:56:11 +0900

    자율학습03

---
+++ src/school/Ex15_1.java
@@ -0,0 +1,17 @@
+package school;
+
+public class Ex15_1 {
+    public static void main(String[] args) {
+        char c = 'A';
+//        byte b = c;
+//        short s = c;
+        int i = c;
+        System.out.println("변수 i_c의 값: " + i);
+        long l = c;
+        System.out.println("변수 l_c의 값: " + l);
+        float f = c;
+        System.out.println("변수 f_c의 값: " + f);
+        double d = c;
+        System.out.println("변수 d_c의 값: " + d);
+    }
+}
