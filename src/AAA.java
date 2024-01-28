4ba546ce9625c6ec322edf0015c7133d1d9c7e87
ddj82 <dnwnrudckf77@gmail.com>
2024-01-28 22:03:21 +0900

    집

--- src/AAA.java
+++ src/AAA.java
@@ -1,19 +1,33 @@
 public class AAA {
 public static void main(String[] args) {
-
-    String str1 = "aaaaa";
-    String str2 = "bbbbb";
-    String s = "";
-
-    for(int i = 0; i < str1.length(); i++) {
-        s += str1.charAt(i);
-        s += str2.charAt(i);
-
+    String code = "abc1abc1abc";
+    String ret = "";
+    int mode = 0;
+    System.out.println(code.substring(0, 1));
+    for (int i = 0; i < code.length(); i++) {
+        if(mode == 0) {
+            if (code.charAt(i) != '1') {
+                if (i % 2 == 0) {
+                    ret += code.charAt(i);
+                }
+            } else {
+                mode = 1;
+                continue;
+            }
+        }
+        if (mode == 1) {
+            if (code.charAt(i) != '1') {
+                if (i % 2 == 1) {
+                    ret += code.charAt(i);
+                }
+            } else {
+                mode = 0;
+                continue;
+            }
+        }
     }
-
-    System.out.println(s);
-
-
+    System.out.println(ret);
+    System.out.println(mode);
     }
 }
 
