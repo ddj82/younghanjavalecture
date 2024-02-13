4eae16a19555ef3802494478261bb4e0fa03f34d
ddj82 <dnwnrudckf77@gmail.com>
2024-02-13 22:33:57 +0900

    집

--- src/AAA.java
+++ src/AAA.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AAA {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 //    String my_string = "BCBdbe";
 //    StringBuffer str = new StringBuffer(my_string);
 //    String letter = "B";
@@ -103,13 +107,114 @@ public static void main(String[] args) {
 //    System.out.println(answer);
 //    String a = s.substring(s.length() - n);
 //    System.out.println(a);
-    int[] a = {1, 2, 3, 100, 99, 98};
-    int[] b = a;
-    for (int i = 0; i < b.length; i++) {
-        System.out.print(b[i] + " ");
+//    int[] a = {1, 2, 3, 100, 99, 98};
+//    int[] b = a;
+//    for (int i = 0; i < b.length; i++) {
+//        System.out.print(b[i] + " ");
+//    }
+    /*
+    String myString = "AbCdEfG";
+    String pat = "aBc";
+    int answer = 0;
+    String a = "1234";
+    String b = pat.toLowerCase();
+    if (a.contains(b)) {
+        answer = 1;
     }
+    int aa = Integer.parseInt(String.valueOf(a.charAt(2)));
+    System.out.println(aa);*/
+
+//    ArrayList<Integer> list = new ArrayList<>();
+//    int[] arr = new int[list.size()];
+//
+//    for (int i = 0; i < 5; i++) {
+//        list.add(i);
+//    }
+//
+//    System.out.println(list);
+//
+//    arr = list.stream()
+//            .mapToInt(Integer::intValue)
+//            .toArray();
+//
+//    System.out.println(Arrays.toString(arr));
+//    String rsp = "205";
+//    String answer = "";
+//    for (int i = 0; i < rsp.length(); i++) {
+//        if (rsp.charAt(i) == 2) {
+//            answer += 0;
+//        } else if (rsp.charAt(i) == 0) {
+//            answer += 5;
+//        } else {
+//            answer += 2;
+//        }
+//    }
+//    System.out.println(answer);
+//    System.out.println(rsp.charAt(0) == '2');
+
+//    int n = 10;
+//    int k = 3;
+//    int[] answer = new int[n / k];
+//
+//    int j = 0;
+//    for (int i = 1; i <= n; i++) {
+//        if (i % k == 0) {
+//            answer[j] = i;
+//            j++;
+//        }
+//    }
+//    System.out.println(Arrays.toString(answer));
+
+//        String is_prefix = "";
+
+//        int answer = 0;
+//        my_string.startsWith(is_prefix);
+//        System.out.println(answer);
+//        String s = "cccCCC";
+
+//        String my_string = "abCdEfghIJ";
+//        String a = "";
+//        for (int i = 0; i < my_string.length(); i++) {
+//            if (Character.isLowerCase(my_string.charAt(i))) {
+//                a += Character.toUpperCase(my_string.charAt(i));
+//            } else if (Character.isUpperCase(my_string.charAt(i))) {
+//                a += Character.toLowerCase(my_string.charAt(i));
+//            }
+//        }
+//        System.out.println(a);
+
+//        String[] strArr = {"AAA","BBB","CCC","DDD"};
+//        for (int i = 0; i < strArr.length; i++) {
+//            if (i % 2 == 0) {
+//                strArr[i] = strArr[i].toLowerCase();
+//            } else {
+//                strArr[i] = strArr[i].toUpperCase();
+//            }
+//        }
+//        System.out.println(strArr[0].toLowerCase());
+//        System.out.println(Arrays.toString(strArr));
+        int answer = 0;
+        int [] arr1 = {100, 17, 84, 1}; //202
+        int [] arr2 = {55, 12, 65, 36}; //168
+        if (arr1.length == arr2.length) {
+            int a1 = 0;
+            int a2 = 0;
+            for (int i = 0; i < arr1.length; i++) {
+                a1 += arr1[i];
+                a2 += arr2[i];
+            }
+            System.out.println(a1);
+            System.out.println(a2);
+            if (a1 > a2) {
+                answer = 1;
+            } else if (a1 < a2) {
+                answer = -1;
+            }
+        }
+        System.out.println(answer);
     }
 }
+
 
 
 
---
+++ src/school_12/javautilex/streamEx.java
@@ -0,0 +1,37 @@
+package school_12.javautilex;
+
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
+
+public class streamEx {
+    public static void main(String[] args) {
+
+        /* 1번
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println(numbers.stream().map(i -> i * 2).collect(Collectors.toList()));
+*/
+
+        /* 2번
+        List<String> words = Arrays.asList("apple", "banana", "orange", "grape", "kiwi");
+        System.out.println(words.stream().filter(i -> i.length() >= 5).collect(Collectors.toList()));
+        words.stream().filter(i -> i.length() >= 5).collect(Collectors.toList());
+        */
+
+        /* 3번
+        List<String> words = Arrays.asList("hello", "world", "java", "stream", "example");
+        System.out.println(words.stream().map(i -> i.toUpperCase()).collect(Collectors.toList()));
+        words.stream().map(i -> i.toUpperCase()).collect(Collectors.toList());
+        */
+
+        /* 4번
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList()));
+        numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+*/
+        List<String> words = Arrays.asList("hello", "world", "java", "stream", "example");
+        System.out.println(words.stream().map(i -> new StringBuffer(i).reverse()).collect(Collectors.toList()));
+        words.stream().map(i -> new StringBuffer(i).reverse()).collect(Collectors.toList());
+
+    }
+}
